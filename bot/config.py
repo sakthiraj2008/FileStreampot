973a12509a04df5445031f7efdb4719754ce2897
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-02-26 02:58:59 -0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 12345))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 5530237028))
-    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234567:xyz")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -100123456789))
+    API_ID = int(env.get("TELEGRAM_API_ID", '11472991'))
+    API_HASH = env.get("TELEGRAM_API_HASH", "c78c50d54baf2173e8b3f75c359c0c72")
+    OWNER_ID = int(env.get("OWNER_ID", '1430742022'))
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "1430742022").split()
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "File2link_gen_tn_bot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "7754306060:AAGBIJsYxqYe9vUPKuCsg5t7i5xS786ufWk")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", '-1002450932371'))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 24))
 
 class Server:
